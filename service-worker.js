9e2ca09404d82823df3484284774b996e7086927
Julen <jfuentes019@ikasle.ehu.eus>
2023-05-02 18:28:23 +0200

    Intento de ServiceWorkers V2

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,62 @@
-'use strict'; // detectará errores de programación
+'use strict'; // Detectara errores de programación
 
-var cacheVersion = 1; // variables que vamos a necesitar
+var cacheVersion = 1; // Variables que vamos a necesitar
 var currentCache = {
     offline: 'offline-cache' + cacheVersion
 };
-const offlineUrl = 'juego-offline.html'; // pagina que queremos cachear
+const offlineUrl = 'juego-offline.html'; // Página que queremos cachear
 
+/*
+ * Función que genera una petición sin pasar por la caché del navegador (evita que tengamos que estar borrándola)
+ */
 function createCacheBustedRequest(url){
     let request= new Request(url, {cache:'reload'});
 
-    if ('cache' in request){
+    if ('cache' in request) { // Si el navegador fuese moderno, con esto sería suficiente
         return request;
     }
 
+    // Para navegadores no-modernos, añadimos un parámetro 'cachebust' con la fecha actual como valor
     let bustedURL= new URL(url, self.location.href);
     bustedURL.search += (bustedURL.search ? '&' : '') + 'cachebust=' + Date.now();
     return request;
 }
 
-this.addEventListener('install', event => { // el evento “install” lo lanza el SW
-    event.waitUntil( // bloqueamos la la instalacion hasta terminar de cachear elementos, si falla la instalacion fallara tambien
+
+this.addEventListener('install', event => { // El evento “install” lo lanza el SW
+    event.waitUntil( // Bloqueamos la instalación hasta terminar de cachear elementos, si falla la instalación fallará también
         caches.open(currentCache.offline).then(function(cache) {
             return cache.addAll([
+                './juego.js',
                 offlineUrl
             ]);
         })
     );
 });
 
+
+/*
+ * Si el usuario lanzar un método GET, el navegador elevará un evento FETCH
+ * El service-worker lo captura (gestor de evento) y...
+ *      - HAY CONEXIÓN --> Devolver lo pedido del servidor
+ *      - NO HAY CONEXIÓN --> Puede devolver algo de la caché o generar algo dinámicamente
+ */
+
 this.addEventListener('fetch', event => {
-    // request.mode = navigate isn't supported in all browsers
-    // so include a check for Accept: text/html header.
+    // 'request.mode = navigate' no está soportado por todos los navegadores, incluimos una segunda opción tras el '||'
     if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
+        event.respondWith( // Respuesta que daremos
+            // Petición al servidor, si hay conexión irá bien, si no (no hay conexion) devolvemos lo que tengamos cacheado
+            // fetch(event.request.url).catch(error => {
             fetch(createCacheBustedRequest(event.request.url)).catch(error => {
-                // Return the offline page
-                return caches.match(offlineUrl);
+                return caches.match(offlineUrl); // Devolvemos lo cacheado
             })
         );
     }
-    else{
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-            .then(function (response) {
-                return response || fetch(event.request);
-            })
-        );
+    else { // Si no es una petición de una página HTML
+        // Si está en caché (response) la devolvemos, si no usamos fetch (vamos a pedirlo online)
+        event.respondWith(caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        }));
     }
 });
